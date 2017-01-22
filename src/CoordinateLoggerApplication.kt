6aa05e5235557f23753efcb5948ee2e4d77de2c9
Matko Smoljan <matko.smoljan2@gmail.com>
2017-01-22 18:44:54 +0100

    Display coordinates in title

--- src/CoordinateLoggerApplication.kt
+++ src/CoordinateLoggerApplication.kt
@@ -6,16 +6,20 @@ import javafx.scene.Parent
 import java.util.*
 
 
-class CoordinateLoggerApplication : Application() {
+class CoordinateLoggerApplication : Application(), CoordinateLoggerController.Listener {
     val initialWidth = 640.0
+
     val initialHeight = 480.0
     val minStageWidth = 460.0
     val minStageHeight = 300.0
-
     val resBundle = "bundles/CoordinateLogger"
     val initialFxml = "CoordinateLogger.fxml"
 
+    var appName: String? = null
+    var stage: Stage? = null
+
     companion object {
+
         @JvmStatic
         fun main(args: Array<String>) {
             launch(CoordinateLoggerApplication::class.java)
@@ -30,10 +34,20 @@ class CoordinateLoggerApplication : Application() {
         val root = fxmlLoader.load<Parent>(javaClass.getResource(initialFxml).openStream())
         val scene = Scene(root, initialWidth, initialHeight)
 
+        appName = languageBundle.getString("app_name")
+
         stage?.minWidth = minStageWidth
         stage?.minHeight = minStageHeight
-        stage?.title = languageBundle.getString("app_name")
-        stage?.setScene(scene)
+        stage?.title = appName
+        stage?.scene = scene
         stage?.show()
+
+        this.stage = stage
+        val controller: CoordinateLoggerController = fxmlLoader.getController()
+        controller.listener = this
+    }
+
+    override fun onImageHoverCoordinatesReceived(x: Double, y: Double) {
+        stage?.title = "$appName ($x, $y)"
     }
 }
