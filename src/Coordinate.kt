204763c67bd13fdcdcf3fc89caf6152fd6be1b52
Matko Smoljan <mats@nodesagency.com>
2018-12-18 10:04:29 +0100

    Removed all project files

--- src/Coordinate.kt
+++
@@ -1,9 +0,0 @@
-data class Coordinate(val x: Int, val y: Int) {
-
-    /**
-     * @return coordinate string in form "x y"
-     */
-    override fun toString(): String {
-        return "$x $y"
-    }
-}
